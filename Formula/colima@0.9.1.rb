a943d9a6ba2cd16929ff048644e3a810a985ddc4
Benjamin Stout <bstout@iconbuild.com>
2026-01-21 15:11:46 -0600

    Add initial formulae

---
+++ Formula/colima@0.9.1.rb
@@ -0,0 +1,43 @@
+class ColimaAT091 < Formula
+  desc "Container runtimes on MacOS (and Linux) with minimal setup"
+  homepage "https://github.com/abiosoft/colima/blob/main/README.md"
+  url "https://github.com/abiosoft/colima.git",
+      tag:      "v0.9.1",
+      revision: "0cbf719f5409ce04b9f0607b681c005d2ff7d94a"
+  license "MIT"
+  head "https://github.com/abiosoft/colima.git", branch: "main"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  depends_on "go" => :build
+  depends_on "lima"
+
+  def install
+    project = "github.com/abiosoft/colima"
+    ldflags = %W[
+      -s -w
+      -X #{project}/config.appVersion=#{version}
+      -X #{project}/config.revision=#{Utils.git_head}
+    ]
+    system "go", "build", *std_go_args(ldflags:), "./cmd/colima"
+
+    generate_completions_from_executable(bin/"colima", shell_parameter_format: :cobra)
+  end
+
+  service do
+    run [opt_bin/"colima", "start", "-f"]
+    keep_alive successful_exit: true
+    environment_variables PATH: std_service_path_env
+    error_log_path var/"log/colima.log"
+    log_path var/"log/colima.log"
+    working_dir Dir.home
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/colima version 2>&1")
+    assert_match "colima is not running", shell_output("#{bin}/colima status 2>&1", 1)
+  end
+end
---
+++ Formula/docker@27.5.1.rb
@@ -0,0 +1,48 @@
+class DockerAT2751 < Formula
+  desc "Pack, ship and run any application as a lightweight container"
+  homepage "https://www.docker.com/"
+  url "https://github.com/docker/cli.git",
+      tag:      "v27.5.1",
+      revision: "9f9e4058019a37304dc6572ffcbb409d529b59d8"
+  license "Apache-2.0"
+  head "https://github.com/docker/cli.git", branch: "master"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)(?:[._-]ce)?$/i)
+  end
+
+  depends_on "go" => :build
+  depends_on "go-md2man" => :build
+  depends_on "docker-completion"
+
+  def install
+    # TODO: Drop GOPATH when merged/released: https://github.com/docker/cli/pull/4116
+    ENV["GOPATH"] = buildpath
+    ENV["GO111MODULE"] = "auto"
+    (buildpath/"src/github.com/docker").install_symlink buildpath => "cli"
+
+    ldflags = %W[
+      -s -w
+      -X github.com/docker/cli/cli/version.BuildTime=#{time.iso8601}
+      -X github.com/docker/cli/cli/version.GitCommit=#{Utils.git_short_head}
+      -X github.com/docker/cli/cli/version.Version=#{version}
+      -X "github.com/docker/cli/cli/version.PlatformName=Docker Engine - Community"
+    ]
+
+    system "go", "build", *std_go_args(ldflags:), "github.com/docker/cli/cmd/docker"
+
+    Pathname.glob("man/*.[1-8].md") do |md|
+      section = md.to_s[/\.(\d+)\.md\Z/, 1]
+      (man/"man#{section}").mkpath
+      system "go-md2man", "-in=#{md}", "-out=#{man}/man#{section}/#{md.stem}"
+    end
+  end
+
+  test do
+    assert_match "Docker version #{version}", shell_output("#{bin}/docker --version")
+
+    expected = "Client: Docker Engine - Community\n Version:    #{version}\n Context:    default\n Debug Mode: false\n\nServer:"
+    assert_match expected, shell_output("#{bin}/docker info", 1)
+  end
+end
