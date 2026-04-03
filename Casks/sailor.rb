e6998532718fab1f57e64085b38471ce8bfe3949
stoutput <ben@stoutput.dev>
2026-04-03 18:01:41 -0500

    Update cask URL/checksums

--- Casks/sailor.rb
+++ Casks/sailor.rb
@@ -1,14 +1,11 @@
 cask "sailor" do
-  arch arm:   "08a4ee2356aa2f1ea3a900d2777bcca98a2028b8861ff2d0d7b954306a0c112f", intel: "cc50507b811415b0261d808687cf6d41176520d5d68c2711f7dc1a562901c021"
+  arch arm: "arm64", intel: "x64"
 
   version "1.0.0"
-  # Update SHA256 hashes when building a new release:
-  #   shasum -a 256 Sailor-<version>-darwin-arm64.zip
-  #   shasum -a 256 Sailor-<version>-darwin-x64.zip
   sha256 arm:   "08a4ee2356aa2f1ea3a900d2777bcca98a2028b8861ff2d0d7b954306a0c112f",
          intel: "cc50507b811415b0261d808687cf6d41176520d5d68c2711f7dc1a562901c021"
 
-  url "https://github.com/stoutput/sailor-desktop/releases/download/v#{version}/Sailor-#{version}-darwin-#{arch}.zip"
+  url "https://github.com/stoutput/sailor-desktop/releases/download/v#{version}/Sailor-darwin-#{arch}-#{version}.zip"
   name "Sailor"
   desc "macOS desktop app for managing Docker containers via Colima"
   homepage "https://github.com/stoutput/sailor-desktop"
